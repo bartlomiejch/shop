1e3e71951048f684e99c6d94263a5a99afaa239e
bartek <bartek.chowaniec@gmail.com>
2017-11-09 21:48:42 +0100

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+from cart.cart import Cart
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    cart = Cart(request)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products,
+                   'cart': cart})
+
+def product_detail(request, id, slug):
+    cart = Cart(request)
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form,
+                   'cart': cart})
